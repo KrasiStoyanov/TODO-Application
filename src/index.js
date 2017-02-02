40f3ae08504e78fd5745aa20bf54171f729a9164
Krasi Stoyanov <krasimir641@gmail.com>
2017-02-02 10:55:04 +0200

    Working example of a ReactJS structure.

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import TodoApp from './containers/TodoApp';
+
+render( <
+    TodoApp / > ,
+    document.getElementById('app')
+);
